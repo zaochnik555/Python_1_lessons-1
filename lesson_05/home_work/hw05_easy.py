73d3927f7d632267dbaf0f459402c111ebb25046
zaochnik555 <zaochnik555@gmail.com>
2018-11-03 09:44:12 +0500

    Выполненная домашняя работа №5 от Сатаева Тимура

--- lesson_05/home_work/hw05_easy.py
+++ lesson_05/home_work/hw05_easy.py
@@ -1,14 +1,60 @@
+import os,sys,shutil
+
 # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+my_path=["dir_" + str(i + 1) for  i in range(9)]
+
+def create_dir(index):
+    dir_path=os.path.join(os.getcwd(),index)
+    try:
+        os.mkdir(dir_path)
+    except:
+        print(dir_path+" - директория с таким именем в этой папке уже есть")
+
+#создадим два варианта скрипта удаления директорий
+def del_dir1(index):
+    dir_path=os.path.join(os.getcwd(),index)
+    try:
+        os.removedirs(dir_path)
+    except:
+        print(dir_path+" - директории с таким именем в этой папке не существует")
+
+def del_dir2(index):
+    dir_path = os.path.join(os.getcwd(), index)
+    try:
+        os.rmdir(dir_path)
+    except:
+        print(dir_path+" - директории с таким именем в этой папке не существует")
+
+for i in my_path:
+    create_dir(i)
+
+for i in my_path:
+    del_dir1(i)
+
+#for i in my_path:
+#    del_dir2(i)
+
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+def print_dir(my_path):
+    for el in os.listdir(my_path):
+        print(el)
 
+my_path = os.getcwd()
+print_dir(my_path)
 
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+def create_copy_of_file(old_file,new_file):
+    shutil.copy(old_file,new_file)
+
+old_file = sys.argv[0]
+new_file = old_file + '.copy_of_file'
+create_copy_of_file(old_file,new_file)
 
--- lesson_05/home_work/hw05_normal.py
+++ lesson_05/home_work/hw05_normal.py
@@ -14,6 +14,43 @@
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
 
+import os,hw05_easy
+
+def change_dir(my_path):
+    try:
+        os.chdir(my_path)
+        print(os.getcwd() + ' - текущая папка')
+    except:
+        print(my_path + ' - такой папки не существует')
+
+
+
+choice="_"
+while choice != "5":
+    print("Меню:")
+    print("1. Перейти в папку")
+    print("2. Просмотреть содержимое текущей папки")
+    print("3. Удалить папку")
+    print("4. Создать папку")
+    print("5. Выход из утилиты")
+    choice = input("Введите номер нужного действия: " )
+    print(choice)
+    if choice == "1":
+        dir_name = input ("Введите полный путь папки: ")
+        change_dir(dir_name)
+    elif choice == "2":
+        dir_name = os.getcwd()
+        hw05_easy.print_dir(dir_name)
+    elif choice == "3":
+        dir_name = input("Введите полный путь папки: ")
+        hw05_easy.del_dir1(dir_name)
+    elif choice == "4":
+        dir_name = input("Введите полный путь папки: ")
+        hw05_easy.create_dir(dir_name)
+    elif choice == "5":
+        pass
+    else:
+        print("Такой пункт меню отсутствует. Попробуйте снова.")
 
 
 
