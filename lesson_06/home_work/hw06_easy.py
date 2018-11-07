bc5afff9912663edca042c5a63dc4b8ebe41a7ed
zaochnik555 <zaochnik555@gmail.com>
2018-11-07 16:35:04 +0500

    Выполненная домашняя работа №6 от Сатаева Тимура

--- lesson_06/home_work/hw06_easy.py
+++ lesson_06/home_work/hw06_easy.py
@@ -1,10 +1,110 @@
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
+import math
+
+class Treug:
+    def __init__(self,x1,y1,x2,y2,x3,y3):
+        self.x1=x1
+        self.y1=y1
+        self.x2=x2
+        self.y2=y2
+        self.x3=x3
+        self.y3=y3
+
+    @staticmethod
+    def length(x1,y1,x2,y2):
+        return math.sqrt(math.pow((x2-x1),2)+math.pow((y2-y1),2))
+
+    @property
+    def ploschad(self):
+        len1=self.length(self.x1, self.y1, self.x2, self.y2)
+        len2=self.length(self.x3, self.y3, self.x2, self.y2)
+        len3=self.length(self.x1, self.y1, self.x3, self.y3)
+        p=(len1+len2+len3)/2
+        return math.sqrt(p*(p-len1)*(p-len2)*(p-len3))
+
+    @property
+    def vysota(self):
+        return self.ploschad/(self.length(self.x1, self.y1, self.x2, self.y2)/2)
+
+    @property
+    def perimetr(self):
+        return self.length(self.x1, self.y1, self.x2, self.y2) +\
+            self.length(self.x3, self.y3, self.x2, self.y2) + \
+            self.length(self.x1, self.y1, self.x3, self.y3)
 
 
+
+print('Задание 1')
+treug1=Treug(5,6,18,7,11,13)
+print('Периметр треугольника: {}'.format(treug1.perimetr))
+print('Площадь треугольника: {}'.format(treug1.ploschad))
+print('Высота треугольника: {}'.format(treug1.vysota))
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 # Предусмотреть в классе методы:
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
+class Trapecia:
+    def __init__(self,x1,y1,x2,y2,x3,y3,x4,y4):
+        self.x1=x1
+        self.y1=y1
+        self.x2=x2
+        self.y2=y2
+        self.x3=x3
+        self.y3=y3
+        self.x4=x4
+        self.y4=y4
 
+        def length(x1,y1,x2,y2):
+            return math.sqrt(math.pow((x2-x1),2)+math.pow((y2-y1),2))
+
+        self.AB = length(self.x1, self.y1, self.x2, self.y2)
+        self.BC = length(self.x2, self.y2, self.x3, self.y3)
+        self.CD = length(self.x3, self.y3, self.x4, self.y4)
+        self.DA = length(self.x4, self.y4, self.x1, self.y1)
+        self.diag_AC = length(self.x3, self.y3, self.x1, self.y1)
+        self.diag_BD = length(self.x2, self.y2, self.x4, self.y4)
+        self.perimeter = self.AB + self.BC + self.CD + self.DA
+
+    def ploschad2(self,len1,len2,len3):
+        p=(len1+len2+len3)/2
+        return math.sqrt(p*(p-len1)*(p-len2)*(p-len3))
+
+    @staticmethod
+    def length(x1,y1,x2,y2):
+        return math.sqrt(math.pow((x2-x1),2)+math.pow((y2-y1),2))
+
+    @property
+    def ploschad(self,x1,y1,x2,y2,x3,y3):
+        len1=self.length(x1, y1, x2, y2)
+        len2=self.length(x3, y3, x2, y2)
+        len3=self.length(x1, y1, x3, y3)
+        p=(len1+len2+len3)/2
+        return math.sqrt(p*(p-len1)*(p-len2)*(p-len3))
+
+    @property
+    def perimetr(self):
+        return self.length(self.x1, self.y1, self.x2, self.y2) + \
+            self.length(self.x2, self.y2, self.x3, self.y3) + \
+            self.length(self.x3, self.y3, self.x4, self.y4) + \
+            self.length(self.x4, self.y4, self.x1, self.y1)
+
+
+
+    # трапеция это два треугольника. Площадь трапеции - это сумма площадей
+    # двух треугольников
+    @property
+    def getPloschad(self):
+        self.area = self.ploschad2(self.AB, self.diag_BD, self.DA) \
+                    + self.ploschad2(self.diag_BD, self.BC, self.CD)
+        return self.area
+
+    def isTrapeciaRavnobocha(self):
+        return self.diag_AC == self.diag_BD
+
+print('/nЗадание 2')
+trap1=Trapecia(5,6,18,7,11,13,7,12)
+print('Периметр трапеции: {}'.format(trap1.perimetr))
+print('Площадь трапеции: {}'.format(trap1.getPloschad))
+print('Равнобочна ли трапеция: {}'.format(trap1.isTrapeciaRavnobocha()))
--- lesson_06/home_work/hw06_normal.py
+++ lesson_06/home_work/hw06_normal.py
@@ -4,10 +4,10 @@
 # У каждого ученика есть два Родителя(мама и папа).
 # Также в школе преподают Учителя. Один учитель может преподавать
 # в неограниченном кол-ве классов свой определенный предмет.
-# Т.е. Учитель Иванов может преподавать математику у 5А и 6Б,
+# Т.е. Учитель Шамсутдинов может преподавать математику у 5А и 6Б,
 # но больше математику не может преподавать никто другой.
 
-# Выбранная и заполненная данными структура должна решать следующие задачи:
+# ВыбрНиная и заполненная данными структура должна решать следующие задачи:
 # 1. Получить полный список всех классов школы
 # 2. Получить список всех учеников в указанном классе
 #  (каждый ученик отображается в формате "Фамилия И.О.")
@@ -16,3 +16,85 @@
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
 
+class People:
+    def __init__(self, name, patronymic, surname):
+        self.name = name
+        self.surname = surname
+        self.patronymic = patronymic
+
+    def get_full_name(self):
+        return self.name + ' ' + self.patronymic + ' ' + self.surname
+
+    def get_short_name(self):
+        return '{} {}.{}.'.format(self.surname.title(), self.name[0].upper(), self.patronymic[0].upper())
+
+
+# if __name__ == '__main__':  # Тестирование.
+#     people1 = People('Руслан', 'Германович', 'Шамсутдинов')
+#     print(people1.get_full_name())
+#     print(people1.get_short_name())
+
+
+class Student(People):
+    def __init__(self, name, patronymic, surname, mom, dad, school_class):
+        People.__init__(self, name, patronymic, surname)
+        self.mom = mom
+        self.dad = dad
+        self.school_class = school_class
+
+
+# if __name__ == '__main__':  # Тестирование.
+#     st_1 = Student('Анвар', 'Владленовна', 'Купцов', 'Нина Сабирова', 'Павел Купцов', '7а')
+#     st_2 = Student('Булат', 'Булатович', 'Сагадатов', 'Регина Рамазанова', 'Григорий Сагадатов', '7б')
+#     st_3 = Student('Марат', 'Азаматович', 'Саакашвилли', 'Эльвира Максимова', 'Марат Саакашвилли', '7а')
+#     print(Student.all_classes(st_1))
+
+
+class Teacher(People):
+    def __init__(self, name, patronymic, surname, subject):
+        People.__init__(self, name, patronymic, surname)
+        self.subject = subject
+
+
+class Class_rooms:
+    def __init__(self, class_room, teachers):
+        self.class_room = class_room
+        self.teachersdict = {t.subject: t for t in teachers}
+
+
+
+if __name__ == '__main__':  # Тестирование.
+    teachers = [Teacher('Руслан', 'Германович', 'Шамсутдинов', 'Математика'),
+                Teacher('Айрат', 'Айратович', 'Саакашвилли', 'Химия'),
+                Teacher('Рустам', 'Рустамович', 'Закиров', 'Правоведение'),
+                Teacher('Азат', 'Азатович', 'Голубев', 'География'),
+                Teacher('Вильдан', 'Маратович', 'Пахомов', 'Ботаника')]
+    classes = [Class_rooms('11 А', [teachers[0], teachers[1], teachers[2]]),
+               Class_rooms('11 Б', [teachers[1], teachers[3], teachers[4]]),
+               Class_rooms('10 А', [teachers[3], teachers[1], teachers[0]])]
+    parents = [People('Анвар', 'Владленовна', 'Купцов'),
+               People('Лилия', 'Аминовна', 'Поезжалова'),
+               People('Альберт', 'Алмазович', 'Платов'),
+               People('Марина', 'Альбертовна', 'Кудрявцева'),
+               People('Булат', 'Булатович', 'Сагадатов'),
+               People('Эльвира', 'Руслановна', 'Рамазанова')]
+    students = [Student('Ирек', 'ГрИрекевич', 'Купцов', parents[0], parents[1], classes[0]),
+                Student('Регина', 'Кудрявцева', 'Кудрявцева', parents[2], parents[3], classes[1]),
+                Student('Инновентий', 'Булатович', 'Сагадатов', parents[4], parents[5], classes[2])]
+    print('Список классов в школе: ')
+    for f in classes:
+        print(f.class_room)
+
+    for f in classes:
+        print('Учителя, преподающие в {} классе:'.format(f.class_room))
+        for teacher in classes[0].teachersdict.values():
+            print(teacher.get_full_name())
+    for f in classes:
+        print("Ученики в классе {}:".format(f.class_room))
+        for st in students:
+            print(st.get_short_name())
+
+    # for f in students:
+    #     print('Список предметов ученика {}'.format(f.school_class))
+    #     for teacher in classes[0].teachersdict.values():
+    #         print(teacher.get_full_name())
